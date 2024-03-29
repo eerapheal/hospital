a3f5138e4d17de7285dc8a3214528d0e46fdc307
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-29 11:24:48 +0530

    global CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        display: ['Inter', 'system-ui', 'sans-serif'],
+        body: ['Inter', 'system-ui', 'sans-serif'],
+      },
+        colors: {
+        primaryColor: "#223740",
+        headingColor: "#223740",
+        smallTextColor: "#6b1d51"
+        },
     },
   },
   plugins: [],
