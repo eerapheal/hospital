d375c1c3a33d058b77a22ec7450c50c1d9624741
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-03-29 11:34:10 +0530

    pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
-import Image from "next/image";
+import HomeCard from "@/components/HomeCard/HomeCard";
+import Contact from "@/components/Contact/Contact";
+import About from "@/components/About/About";
+import Products from "@/components/Products/Produtcs";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="">
-        page   
-      </div>
+    <main className="" style={{
+      backgroundImage:
+        "linear-gradient(80deg, #eef7fb 0 60%,  #C9F6FF 0% 100%)",
+      paddingTop: "60px",
+      minHeight: "100vh",
+      overflow: "hidden",
+    }}>
+      <HomeCard />
+      <Products />
+      <About />
+      <Contact />
     </main>
   );
 }
